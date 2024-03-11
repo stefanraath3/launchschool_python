9b720632c6eb32fd1732f5646b7de6449f780c9e
Stefan Raath <stefanraath3@gmail.com>
2024-03-11 17:50:22 +0200

    Add exercise 10

--- loops_iterating/exercises.py
+++ loops_iterating/exercises.py
@@ -240,6 +240,31 @@ def factorial(n):
         n -= 1
 
     return result 
+------------------------------
+------------------------------
+10. The following code uses the randrange function from Python's random library
+ to obtain and print a random integer within a given range. 
+ Using a while loop, it keeps running until it finds a random number 
+ that matches the last number in the range. 
+ Refactor the code so it doesn't require two different invocations of randrange.
+
+import random
+highest = 10
+number = random.randrange(highest + 1)
+print(number)
+
+while number != highest:
+    number = random.randrange(highest + 1)
+    print(number)
 '''
 
+
+import random
+highest = 10
+
+while True:
+    number = random.randrange(highest +1)
+    print(number)
+    if number == highest:
+        break
 
