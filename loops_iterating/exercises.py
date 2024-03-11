6fb0deadc013a94f5d7e9ba7a4dc2d03ce854d01
Stefan Raath <stefanraath3@gmail.com>
2024-03-11 15:09:28 +0200

    add exercise 9

--- loops_iterating/exercises.py
+++ loops_iterating/exercises.py
@@ -187,7 +187,59 @@ my_dict = {
 print(my_dict)   
 ------------------------------
 ------------------------------
+9. Write a function that computes and returns the factorial of a number
+ by using a for or while loop. The factorial of a positive integer n,
+  signified by n!, is defined as the product of all integers between 1 and n, inclusive:
 
+n!	Expansion	Result
+1!	1 => 1
+2!	1 * 2 => 2
+3!	1 * 2 * 3 => 6
+4!	1 * 2 * 3 * 4 => 24
+5!	1 * 2 * 3 * 4 * 5 => 120
+You may assume that the argument is always a positive integer.
 
+Expected output
+print(factorial(1))   # 1
+print(factorial(2))   # 2
+print(factorial(3))   # 6
+print(factorial(4))   # 24
+print(factorial(5))   # 120
+print(factorial(6))   # 720
+print(factorial(7))   # 5040
+print(factorial(8))   # 40320
+print(factorial(25))  # 15511210043330985984000000
+
+------------------------------
+My solution:
+- function that computes and returns factorial of a number
+- use a for loop or a while loop
+- factorial n! is the * of all integers between 1 and n, inclusive 
+- the number of elements being multiplied is the same as n, ex 5! has 5 elements being multiplied (1 to 5)
+
+def factorial(number): 
+    result = 1
+    for i in range(1, number + 1):
+        result *= i
+    return result
+
+print(factorial(4))
+
+Aaahhh okay so the for loop from the solution basically creates a reversed range, which makes sense
+def factorial(n):
+    result = 1
+    for number in range(n, 0, -1):
+        result *= number
+
+    return result
+
+def factorial(n):
+    result = 1
+    while n > 0:
+        result *= n
+        n -= 1
+
+    return result 
 '''
+
 
