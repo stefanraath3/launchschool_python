e58363f4c4fee6db267cb0abd31828a59077d3cd
Stefan Raath <stefanraath3@gmail.com>
2024-03-11 11:54:25 +0200

    add ex 5. from loops and iterating chapter

--- loops_iterating/exercises.py
+++ loops_iterating/exercises.py
@@ -16,7 +16,8 @@ Expected Odd Values
 3
 11
 17
-'''
+------------------------------
+My Solution:
 my_list = [6, 3, 0, 11, 20, 4, 17]
 index = 0
 
@@ -28,4 +29,41 @@ while index < len(my_list):
 for number in my_list:
     if number % 2 != 0:
         print(number)
-    
+'''
+
+
+'''
+5.Print all of the even numbers 
+in the following list of nested lists.
+ Don't use any while loops. 
+
+my_list = [
+    [1, 3, 6, 11],
+    [4, 2, 4],
+    [9, 17, 16, 0],
+]
+
+Expected Output:
+6
+4
+2
+4
+16
+0
+
+------------------------------
+My Solution:
+my_list = [
+    [1, 3, 6, 11],
+    [4, 2, 4],
+    [9, 17, 16, 0],
+]
+
+for sublist in my_list:
+    for number in sublist:
+        if number % 2 == 0: 
+            print(number)
+
+
+'''
+
