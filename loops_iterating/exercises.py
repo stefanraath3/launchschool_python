1900e4dbf4c835fcaf986dff88e24fbced090754
Stefan Raath <stefanraath3@gmail.com>
2024-03-11 18:50:30 +0200

    Add exercise 11

--- loops_iterating/exercises.py
+++ loops_iterating/exercises.py
@@ -256,8 +256,9 @@ print(number)
 while number != highest:
     number = random.randrange(highest + 1)
     print(number)
-'''
 
+------------------------------
+My solution: 
 
 import random
 highest = 10
@@ -267,4 +268,63 @@ while True:
     print(number)
     if number == highest:
         break
+------------------------------
+------------------------------
+11. Print all of the even numbers in the following list of nested lists.
+This time, don't use any for loops.
+
+my_list = [
+  [1, 3, 6, 11],
+  [4, 2, 4],
+  [9, 17, 16, 0],
+]
+
+Expected Output
+6
+4
+2
+4
+16
+0
+------------------------------
+
+My Solution: 
+
+my_list = [
+  [1, 3, 6, 11, 24],
+  [4, 2, 4],
+  [9, 17, 16, 0],
+]
+
+outer_index = 0
+
+while outer_index < len(my_list):
+    inner_list_length = len(my_list[outer_index]) 
+    inner_index = 0 
+    while inner_index < inner_list_length:
+        if my_list[outer_index][inner_index] % 2 == 0:
+           print(my_list[outer_index][inner_index])
+        inner_index += 1
+    outer_index += 1
+
+LS Solution: 
+my_list = [
+  [1, 3, 6, 11],
+  [4, 2, 4],
+  [9, 17, 16, 0],
+]
+
+outer_index = 0
+while outer_index < len(my_list):
+    inner_index = 0
+    while inner_index < len(my_list[outer_index]):
+        number = my_list[outer_index][inner_index]
+        if number % 2 == 0:
+            print(number)
+
+        inner_index += 1
+
+    outer_index += 1
+'''
+
 
