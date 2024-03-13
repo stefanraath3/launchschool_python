fa0e113d1a65ee71ff4edf80495f5bb99a6bf553
Stefan Raath <stefanraath3@gmail.com>
2024-03-13 12:22:03 +0200

    Add exercise 4

--- variables_as_pointers/exercises.py
+++ variables_as_pointers/exercises.py
@@ -45,10 +45,32 @@ dict2['Monty Python'] = 'Holy Grail'
 print(dict1['Monty Python'])
 
 ------------------------------
+My Solution: 
 'The Life of Brian',
 A shallow copy of dict1 is being made with the dict constructor. When shallow copies are made, the outermost level
 of the original object is copied. So in this case, since dict2 is a new variable in the stack, they are not the same object.
 Changes to dict2 does not influence dict1 because they are distinct objects, even though they have the same values. 
 dict2 was assigned dict1, so references to the same object weren't copied. in this case, dict2 is references a different 
 object than dict1
+------------------------------
+------------------------------
+4. Without running this code, what will it print? Why?
+
+dict1 = {
+    'a': [1, 2, 3],
+    'b': (4, 5, 6),
+}
+
+dict2 = dict(dict1)
+dict1['a'][1] = 42
+print(dict2['a'])
+------------------------------
+My Solution: 
+[1, 42, 3]
+Changes made to the nested objects of dict1 will be reflected in dict2, because a shallow copy has been done of dict1 to 
+create the object that dict2 references. These are two distinct objects, however their nested lists and tuples are not.
+Only the heap memory addresses of the nested mutable objects were copied, and 
+not the objects themselves. Therefore, dict1 and dict2 share the same references to the same nested objects at the same
+heap memory locations. Thus, changes made to these nested objects will be reflected in both variables.
+
 '''
